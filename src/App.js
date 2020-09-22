679ff1e16f3c5a43a2d27d55fe67822868a0b468
Aman0819 <amansingh011999@gmail.com>
2020-09-22 19:14:16 +0530

    firsttttttt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sub from "./components/Sub";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Sub
+          name="unixporn"
+          url="https://mudittiwari2000.github.io/Animated-Card/imgs/card-main.png"
+          alt="lmao"
+          subcount="69k"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+        illo molestiae maiores debitis culpa aspernatur eos."
+          rank="7"
+        />
+        <Sub
+          name="unixporn"
+          url="https://mudittiwari2000.github.io/Animated-Card/imgs/card-main.png"
+          alt="lmao"
+          subcount="69k"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+        illo molestiae maiores debitis culpa aspernatur eos."
+          rank="7"
+        />
+        <Sub
+          name="unixporn"
+          url="https://mudittiwari2000.github.io/Animated-Card/imgs/card-main.png"
+          alt="lmao"
+          subcount="69k"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+        illo molestiae maiores debitis culpa aspernatur eos."
+          rank="7"
+        />
+      </div>
     </div>
   );
 }
